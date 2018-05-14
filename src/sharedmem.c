c9902a210320d2e2b554c8e54f533545c33e0cc3
Ovidiu - Dan Bogat <ic17b501@technikum-wien.at>
2018-05-14 23:37:42 +0200

    Handling fuer sharedmem als statisches Library hinzugefuegt (skeleton)

---
+++ src/sharedmem.c
@@ -0,0 +1,17 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sharedmem.c
+ *
+ *    Description:  Handlig fuer /dev/shm 
+ *
+ *        Version:  1.0.0
+ *        Created:  04/30/2018 10:20:05 AM
+ *       Revision:  none
+ *       Compiler:  gcc52
+ *
+ *         Author:  Ovidiu - Dan Bogat [ic17b501], ic17b501@technikum-wien.at
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include "sharedmem.h"
---
+++ src/sharedmem.h
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sharedmem.h
+ *
+ *    Description:  Header fuer /dev/shm handling
+ *
+ *        Version:  1.0.0
+ *        Created:  04/30/2018 10:20:05 AM
+ *       Revision:  none
+ *       Compiler:  gcc52
+ *
+ *         Author:  Ovidiu - Dan Bogat [ic17b501], ic17b501@technikum-wien.at
+ * =====================================================================================
+ */
+#ifndef __sharedmem_h
+#define __sharedmem_h
+
+
+#endif /* sharedmem.h */
