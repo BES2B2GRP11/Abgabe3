02eaf86e8fff973cf15411443c9f807650c1c6ae
Ovidiu-Dan Bogat <ic17b501@technikum-wien.at>
2018-05-26 17:02:20 +0200

    Uebersicht in Sourcefiles eingepflegt

--- src/empfaenger.c
+++ src/empfaenger.c
@@ -19,6 +19,53 @@
 #include "empfaenger.h"
 #include "ringbuffer.h"
 
+/*                          +----------------------------------------------------------------------------------+ */
+/*                          |  [Datei mit n fixen Bloecken in /dev/shm als circular-linked-list] == ringbuffer | */
+/*                          |  Ist fuer beide Programme gleich /dev/shm/shm_{1000*uid+0}                       | */
+/*                          |                                                                                  | */
+/*                          |       1      2      3      4      5      6                                   n   | */
+/*                          |     +---+  +---+  +---+  +---+  +---+  +---+                               +---+ | */
+/*                          |     |   |  |   |  |   |  |   |  |   |  |   |                               |   | | */
+/*                          +---> |   +> |   +> |   +> |   +> |   +> |   +--------> . . . . . . . +----> |   +-+ */
+/*                                +-+-+  +-+-+  +-+-+  +-+-+  +-+-+  +-+-+                               +-+-+ */
+/*                                  ^      ^      ^      ^      ^      ^                                   ^ */
+/*                                  |      |      |      |      |      |                                   | */
+/*                                  |      |      |      |      |      |                                   | */
+/*                                  +      +      +      +      +      +                                   + */
+/*                                  p      p      p      p      p      p                                   p */
+
+/*                                +----------------------------------------------------------+------------------+ */
+/*                                |                                                          |                  | */
+/*                                |  [Datei mit n fixen blöcken]+>/de^/shm/shm_{1000*uid+j}  |  /dev/shm @ RAM  | */
+/*                                |                                                          |                  | */
+/*                                +------------+-+-------------------------------------------+------------------+ */
+/*                                             ^ ^ */
+/*                                             | | */
+/* +-----------------------------+             | |                                  +------------------------------+ */
+/* |   Sender                    |             | |                                  |                    Empfaenger| */
+/* +-----------------------------+             | |                                  +------------------------------+ */
+/* |                             |             | |                                  |                              | */
+/* | int fd = /dev/shm/shm_...   |             | |                                  |  int fd = /dev/shm/shm_...   | */
+/* |                             +-------------+ +----------------------------------+                              | */
+/* | Complex+Semaphore+up()      |                                                  |  Complex+Semaphore+up()      | */
+/* |                             | Alle Arbeiten mit den gleichen Dateien.          |                              | */
+/* | write to ringbuffer@dev/shm | Bei uns macht es die Reihenfolge:                |  read from ringbuffer@dev/shm| */
+/* |                             |                                                  |                              | */
+/* | Complex+Semaphore+down()    | /dev/shm_{1000*uid+j} -> j = 0 ==> ringbuffer    |  Complex+Semaphore+down()    | */
+/* |                             |                                                  |                              | */
+/* | atexit(cleanup)             | Fuer Semaphore:                                  |  atexit(cleanup)             | */
+/* +-----------------------------+ /de /sem_{1000*uid+j} -> j = 0 --> complex-sem.  +------------------------------+ */
+/*                                 /dev/sem_{1000*uid+j} -> j = 1 --> ring buffer sem */
+
+/*                                 Der Complex-Semaphore gibt an, wie viele threads */
+/*                                 bzw. Prozesse sich in der geschuetzten Region befinden. */
+/*                                 Diese wird ausgewertet beim Abbau und Loeschen des Buffers: */
+/*                                 Empfaenger ist der letzte, der noch mit dem buffer arbeitet, */
+/*                                 somit kann er nach dem letzten auslesen auch geloescht werden. */
+
+/*                                 Der Ring buffer Semaphore wird verwendet um das Lesen und Schreiben */
+/*                                 in dem Ringbuffer zu Synchronisieren. */
+
 int main(int argc, char** argv)
 {
   argc=argc;
